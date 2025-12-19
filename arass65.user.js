0ff651cc123d2fb525a41c4caac8734380bb324b
aras65x <azemaras306@gmail.com>
2025-12-19 09:01:56 +0300

    Add UserScript metadata for AgarZ Aras65
    Add metadata for UserScript including name, version, and description.

--- arass65.user.js
+++ arass65.user.js
@@ -1,1 +1,11 @@
+// ==UserScript==
+// @name         AgarZ Aras65
+// @namespace    https://agarz.com/
+// @version      1.3
+// @description  Online güncellenen AgarZ sistemi
+// @match        https://agarz.com/*
+// @grant        none
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/aras65x/agarz-aras65-systm/main/system.js
+// ==/UserScript==
 
